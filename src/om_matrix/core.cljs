3c021d2cf1195eb6194581aecf0817b6c9f40d4e
Chris Jennings <boz@cox.net>
2015-12-26 08:50:03 -0800

    2x2 matrix db with :matrix, :row and :cell. only does render, no mutate.
    Here I'm trying to image what a db of the matrix might be like. I don't think it's right. Extracting the data from the db is too complicated. It may be simpler to represent the cells as a series and not include the row and matrix info. Or maybe just include the dimensions of the matrix, plus the cells.

--- src/om_matrix/core.cljs
+++ src/om_matrix/core.cljs
@@ -24,7 +24,7 @@
   (let [st @state]
     (if-let [[_ v] (find st key)]
       {:value v}
-            {:value :not-found})))
+      {:value :not-found})))
 
 (def parser (om.next/parser {:read read}))
 
@@ -41,47 +41,76 @@
 
 ;; ------------------------ scratch -------------
 
-(def init-state
-  {:board [[1 2]
-           [3 4]]})
-
-(defmulti read om/dispatch)
-
-(defmethod read :board
-  [{:keys [state] :as env} key params]
-  {:value (key state)})
-
-(defmethod read :default
-  [{:keys [state] :as env} key params]
-  {:value {:value (str "no-value-for-key: " key)}})
+(def state-matrix
+  {:matrix [[:row 1]
+            [:row 2]]
+   :row {1 [[:cell 1] [:cell 2]]
+         2 [[:cell 3] [:cell 4]]}
+   :cell {1 {:id 1 :state :x}
+          2 {:id 2 :state :o}
+          3 {:id 3 :state :x}
+          4 {:id 4 :state :o}}})
 
 (defui Cell
-  static om/Ident
-  (ident [this props]
-         [:board/cell])
-  static om/IQuery
-  (query [this]
-         `[:cell])
+  ;; static om/Ident
+  ;; (ident [this props]
+  ;;        (let [id (:id props)]
+  ;;          (println "Cell.ident: id: " id)
+  ;;          `[[:cell ~id]]))
+  ;; static om/IQuery
+  ;; (query [this]
+  ;;        (println "Cell.query: " (om/props this))
+  ;;        [:cell])
   Object
   (render [this]
-          (dom/div nil "cell")))
+          (let [{:keys [id state] :as datum} (om/props this)]
+            (println "Cell: " (om/props this))
+            (dom/div #js {:className "cell"} (name state)))))
 
 (def cell (om/factory Cell))
-;(cell)
 
-(defui Board
-  static om/IQuery
-  (query [this]
-         (let [subquery (om/get-query Cell)]
-           `[{:board ~subquery}])))
+(defui Row
+  Object
+  (render [this]
+          (let [{:keys [row-id state]} (om/props this)
+                row (:row state)
+                cell-links (get row row-id)
+                cell-ids (map second cell-links)
+                cell-data (map (fn [id] (get (:cell state) id)) cell-ids)]
+            (println "Row: " (om/props this))
+            ;; (println "Row: row-id: " row-id)
+            ;; (println "Row: row: " row)
+            ;; (println "Row: cell-links: " cell-links)
+            ;; (println "Row: cell-ids: " cell-ids)
+            (dom/div #js {:className "row"}
+                     (map cell cell-data)))))
 
-(def board (om/factory Board))
+(def row (om/factory Row))
+
+(defui Matrix
+  ;; static om/IQuery
+  ;; (query [this]
+  ;;        (let [subquery (om/get-query Cell)]
+  ;;          `[{:board ~subquery}]))
+  Object
+  (render [this]
+          (let [{:keys [matrix] :as state} (om/props this)]
+            (println "Matrix: " (om/props this))
+            (dom/div #js {:className "matrix"}
+                     (map #(row {:row-id (second %) ; <- the row state is weird, second gets the id
+                                 :state state
+                                 }) matrix))))
+  )
 
 
-(def norm-d (om/tree->db Board init-state true))
-norm-d
+(def matrix (om/factory Matrix))
 
-(def parser2 (om/parser {:read read}))
-(parser {:state (atom norm-d)} '[:board])
+(def reconciler-matrix
+  (om/reconciler {:state state-matrix}))
 
-(om/get-query Cell)
+(defn main-matrix []
+  (println "main-matrix ---------------------------------")
+  (om/add-root! reconciler-matrix
+                Matrix (gdom/getElement "scratch")))
+
+(main-matrix)
