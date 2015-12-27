252f49e7a648168923f67e8479e75fe43e42c59d
Chris Jennings <boz@cox.net>
2015-12-27 11:43:00 -0800

    Trying simple nested vectors and imagined db structure for matrix.

--- src/om_matrix/core.cljs
+++ src/om_matrix/core.cljs
@@ -52,25 +52,36 @@
           3 {:id 3 :state :x}
           4 {:id 4 :state :o}}})
 
+(def state-matrix-simple
+  {:matrix [[{:id 1 :state :x} {:id 2 :state :o}]
+            [{:id 3 :state :x} {:id 4 :state :o}]]})
+
 (defui Cell
-  ;; static om/Ident
-  ;; (ident [this props]
-  ;;        (let [id (:id props)]
-  ;;          (println "Cell.ident: id: " id)
-  ;;          `[[:cell ~id]]))
-  ;; static om/IQuery
-  ;; (query [this]
-  ;;        (println "Cell.query: " (om/props this))
-  ;;        [:cell])
+  static om/Ident
+  (ident [this props]
+         (let [id (:id props)]
+           (println "Cell.ident: id: " id)
+           `[:cell ~id]))
+  static om/IQuery
+  (query [this]
+         [:id :state])
   Object
   (render [this]
           (let [{:keys [id state] :as datum} (om/props this)]
             (println "Cell: " (om/props this))
             (dom/div #js {:className "cell"} (name state)))))
 
-(def cell (om/factory Cell))
+(def cell (om/factory Cell {:keyfn :id}))
 
 (defui Row
+  static om/Ident
+  (ident [this props]
+         (let [id (:id props)]
+           (println "Row.ident: id: " id)
+           `[:row ~id]))
+  static om/IQuery
+  (query [this]
+         `[{:cell ~(om/get-query Cell)}])
   Object
   (render [this]
           (let [{:keys [row-id state]} (om/props this)
@@ -84,35 +95,51 @@
             ;; (println "Row: cell-links: " cell-links)
             ;; (println "Row: cell-ids: " cell-ids)
             (dom/div #js {:className "row"}
-                     (map cell cell-data)))))
+                     (map cell cell-data)
+                     ))))
 
-(def row (om/factory Row))
+
+(def row (om/factory Row {:keyfn :row-id}))
 
 (defui Matrix
-  ;; static om/IQuery
-  ;; (query [this]
-  ;;        (let [subquery (om/get-query Cell)]
-  ;;          `[{:board ~subquery}]))
+  static om/Ident
+  (ident [this props]
+         (println "Matrix.ident: props: " (pprint props))
+         [:matrix])
+  static om/IQuery
+  (query [this]
+         `[{:matrix [{:row ~(om/get-query Row)}]}])
   Object
   (render [this]
           (let [{:keys [matrix] :as state} (om/props this)]
-            (println "Matrix: " (om/props this))
+            (println "Matrix.render:: props: " (om/props this))
             (dom/div #js {:className "matrix"}
                      (map #(row {:row-id (second %) ; <- the row state is weird, second gets the id
-                                 :state state
-                                 }) matrix))))
-  )
+                                 :state state})
+                          matrix)))))
 
 
 (def matrix (om/factory Matrix))
 
+(defn read-matrix [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      (do
+        (println "read:: key: " key " || state: " state " || params: " params)
+        {:value value})
+      {:value :not-found})))
+
 (def reconciler-matrix
-  (om/reconciler {:state state-matrix}))
+  (om/reconciler {
+                  ;;:state (atom state-matrix-db)
+                  :state (atom state-matrix-tree)
+                  :parser (om/parser {:read read-matrix})}))
 
 (defn main-matrix []
   (println "main-matrix ---------------------------------")
   (om/add-root! reconciler-matrix
-                Matrix (gdom/getElement "scratch")))
+                Matrix
+                (gdom/getElement "scratch")))
 
 (main-matrix)
 
